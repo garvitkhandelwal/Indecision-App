a2910160673285fd4794f1f8666f024f970ae41e
Garvit Khandelwal <garvitkhandelwal14@gmail.com>
2018-08-16 00:03:42 +0530

    Added new file

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,18 +1,6 @@
 'use strict';
 
-var nameVar = 'Garvit';
-var nameVar = 'Aman';
-console.log('nameVar', nameVar);
-
-var nameLet = 'Jane';
-nameLet = 'Julie';
-console.log('nameLet', nameLet);
-
-var nameConst = 'Rachel';
-console.log('nameConst', nameConst);
-
-var fullName = 'Joey Tribbiani';
-if (fullName) {
-    var firstName = fullName.split(' ')[0];
+function square(number) {
+    if (number) return number * number;else return 'No number';
 }
-console.log('firstName', firstName);
+console.log(square(10));
---
+++ indecision-app/src/playground/es6-arrow-functions.js
@@ -0,0 +1,7 @@
+function square(number){
+    if(number)
+        return number*number;
+    else
+        return 'No number';
+}
+console.log(square(10));
