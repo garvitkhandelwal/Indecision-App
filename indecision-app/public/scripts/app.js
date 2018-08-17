c34f7d5165adf12880ec0b6ef53c082137a5b268
Garvit Khandelwal <garvitkhandelwal14@gmail.com>
2018-08-18 01:55:55 +0530

    changed files

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -10,6 +10,15 @@ var app = {
     options: ['Option 1', 'Option 2']
 };
 
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+    app.options.push(option);
+    e.target.elements.option.value = null;
+    console.log(app.options);
+};
+
 var template = React.createElement(
     'div',
     null,
@@ -27,7 +36,18 @@ var template = React.createElement(
         'p',
         null,
         app.options.length > 0 ? 'There are some options.' : 'No options'
+    ),
+    React.createElement(
+        'form',
+        { onSubmit: onFormSubmit },
+        React.createElement('input', { type: 'text', name: 'option' }),
+        React.createElement(
+            'button',
+            null,
+            'Add option'
+        )
     )
 );
 
 var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -8,14 +8,26 @@ var app = {
     options : ['Option 1', 'Option 2']
 };
 
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    
+    const option = e.target.elements.option.value;
+    app.options.push(option);
+    e.target.elements.option.value = null;
+    console.log(app.options);
+};
+
 var template = (
     <div>
         <h1>{app.title}</h1>
         {app.subtitle && <p>{app.subtitle}</p>}
         <p>{app.options.length > 0 ? 'There are some options.' : 'No options'}</p>
+        <form onSubmit={onFormSubmit}>
+            <input type='text' name='option'/>
+            <button>Add option</button>
+        </form>
     </div>
 );
 
 var appRoot = document.getElementById('app');
-
-
+ReactDOM.render(template,appRoot);
